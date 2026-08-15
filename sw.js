adc69812c400fe3ec86994724f45cc23d7ef2b79
ricky460pt <ricardo460cv@gmail.com>
2026-08-15 04:38:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const C='dv5';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg','./sw.js']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
+const C='dv51-20260815';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){
+   e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+ } else {
+   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+ }
+});
