652da09bf2da72795e0558ea4f1f9d9df744f8b8
ricky460pt <ricardo460cv@gmail.com>
2026-08-15 05:34:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,16 @@
-const CACHE_NAME = "diario-pro-v5.4";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icone.svg"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-  self.skipWaiting();
+const CACHE="diario-pro-v5-3";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icone.svg"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{
+    const copy=resp.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy));
+    return resp;
+  }).catch(()=>cached)));
 });
