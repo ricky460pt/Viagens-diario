62f9bd2053bedf72cf74f386b44832a0a61fb93c
ricky460pt <ricardo460cv@gmail.com>
2026-08-15 13:14:23 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const C='dv51-20260815';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){
+   e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+ } else {
+   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+ }
+});
